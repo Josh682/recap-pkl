b8a4ccb916519b02ff574ca8217c2d39c2e567d6
joshua kusuma <joshuakusuma682@gmail.com>
2024-01-24 22:36:23 +0700

    homepage ui

---
+++ src/components/contentGrid.svelte
@@ -0,0 +1,3 @@
+<div class={`grid grid-cols-1 place-items-center ${$$props.class}`}>
+    <slot/>
+</div>
---
+++ src/components/tes.svelte
@@ -0,0 +1,3 @@
+<div class={`w-[75vw] h-[100px] border border-black rounded-xl${$$props.class}`}>
+    <slot/>
+</div>
